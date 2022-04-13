ce7d5ae53fa1861789e51ea22922ce7dcabebbd1
IfedayoAwe <olalekanawe99@gmail.com>
2022-04-13 19:19:18 +0100

    Api commits

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from blog.api.views import post_detail, ApiBlogListView
+from blog.api.views import new_post, post_detail, ApiBlogListView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('post/<int:pk>/', post_detail, name='post-detail'),
+    path('post/', new_post, name='post'),
     path('', ApiBlogListView.as_view(), name='list')
 ]
--- blog/api/views.py
+++ blog/api/views.py
@@ -21,6 +21,19 @@ class ApiBlogListView(ListAPIView):
     permission_classes = [IsAuthenticated|ReadOnly]
     pagination_class = PageNumberPagination
 
+@api_view(['GET', 'POST'])
+@permission_classes((IsAuthenticated,))
+def new_post(request):
+
+    if request.method == 'POST':
+        user = request.user
+        post = Post(author=user)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
 @api_view(['GET', 'PUT', 'DELETE'])
 @permission_classes((IsAuthenticated,))
 def post_detail(request, pk):
