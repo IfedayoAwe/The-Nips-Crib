5f6880468687e244700fabc39844577ce4aedd21
IfedayoAwe <olalekanawe99@gmail.com>
2022-04-13 18:22:30 +0100

    Update urls.py

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -4,7 +4,6 @@ from blog.api.views import post_list, post_detail, ApiBlogListView
 app_name = 'blog'
 
 urlpatterns = [
-    path('', post_list, name='blog-home'),
     path('post/<int:pk>/', post_detail, name='post-detail'),
-    path('list/', ApiBlogListView.as_view(), name='list')
+    path('', ApiBlogListView.as_view(), name='list')
 ]
